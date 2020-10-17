87b03f2952ab3edcf32c2442c19182c3a4bebe82
Chase Spencer <cah-chase-spencer@cardinalhealth.com>
2020-10-17 17:25:19 -0400

    init poc code, still needs Postgres connection

---
+++ src/main/java/com/productpickerservicepoc/models/Product.java
@@ -0,0 +1,71 @@
+package com.productpickerservicepoc.models;
+
+import org.springframework.core.style.ToStringCreator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    public Long id;
+
+    @Column
+    public String name;
+
+    @Column
+    public String shortdescription;
+
+    @Column
+    public String imageurl;
+
+    @Column
+    public Integer quantity;
+
+    @Column
+    public Double price;
+
+    public Long getId() {return this.id;}
+    public void setId(Long id) { this.id = id;}
+
+    public String getName() {return this.name;}
+    public void setName(Long id) { this.name = name;}
+
+    public String getShortDescription() {return this.shortdescription;}
+    public void setShortDescription(String shortDescription) { this.shortdescription = shortDescription;}
+
+    public String getImageUrl() {return this.imageurl;}
+    public void setImageUrl(String imageUrl) { this.imageurl = imageUrl;}
+
+    public Integer getQuantity() {return this.quantity;}
+    public void setQuantity(Integer quantity) { this.quantity = quantity;}
+
+    public Double getPrice() {return this.price;}
+    public void setPrice(Double price) { this.price = price;}
+
+    public Product(final Long id, final String name,
+                   final String shortdescription, final String imageurl,
+                   final Integer quantity, final Double price) {
+        this.id = id;
+        this.name = name;
+        this.shortdescription = shortdescription;
+        this.imageurl = imageurl;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringCreator(this)
+                .append("id", this.getId())
+                .append("name", this.getName())
+                .append("shortdescription", this.getShortDescription())
+                .append("imageurl", this.getImageUrl())
+                .append("quantity", this.getQuantity())
+                .append("price", this.getPrice())
+                .toString();
+    }
+}
---
+++ src/main/java/com/productpickerservicepoc/providers/GraphQLDataFetchers.java
@@ -0,0 +1,32 @@
+package com.productpickerservicepoc.providers;
+
+import com.google.common.collect.ImmutableMap;
+import graphql.schema.DataFetcher;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class GraphQLDataFetchers {
+    private static List<Map<String, String>> products = Arrays.asList(
+            ImmutableMap.<String, String>builder()
+                    .put("id", "id")
+                    .put("name", "name")
+                    .put("shortdescription", "shortdescription")
+                    .put("imageurl", "imageurl")
+                    .put("quantity", "quantity")
+                    .put("price", "price")
+            .build());
+    public DataFetcher getProductsById() {
+        return dataFetchingEnvironment -> {
+            String productId = dataFetchingEnvironment.getArgument("id");
+            return products
+                    .stream()
+                    .filter(product -> product.get("id").equals(productId))
+                    .findFirst()
+                    .orElse(null);
+        };
+    }
+}
