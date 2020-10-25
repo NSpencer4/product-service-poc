fb8236a5a86b7c9a53ef1fb9c28fb0f1047681d9
Chase Spencer <cah-chase-spencer@cardinalhealth.com>
2020-10-24 23:06:35 -0400

    Main test

--- src/test/java/com/productservicepoc/ProductServiceApplicationTests.java
+++ src/test/java/com/productservicepoc/ProductServiceApplicationTests.java
@@ -1,13 +1,24 @@
 package com.productservicepoc;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.SpringApplication;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
 class ProductServiceApplicationTests {
+	@Mock
+	private SpringApplication springApplication = new SpringApplication();
+	@InjectMocks
+	private ProductServiceApplication app;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(app);
 	}
 
 }
